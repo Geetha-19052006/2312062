934a4a1176c4334b91616c5e626e67db42062e5b
Geetha-19052006 <2312062@nec.edu.in>
2026-07-02 12:57:31 +0530

    Completed notification app backend, frontend setup and logging middleware

--- notification-app-fe/src/api/notifications.js
+++ notification-app-fe/src/api/notifications.js
@@ -1,2 +1,8 @@
-export function fetchNotifications() {
-}
+import axios from "axios";
+
+const API = "http://localhost:3000/notifications";
+
+export const getNotifications = async () => {
+  const response = await axios.get(API);
+  return response.data;
+};
